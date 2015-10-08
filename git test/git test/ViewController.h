ddbba3fdf7a1a17f09237f616e3d3012a9797374
陈震 <Ray@deMacBook-Air.local>
2015-10-08 14:06:00 +0800

    ewer

---
+++ git test/git test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git test
+//
+//  Created by 陈震 on 15/10/8.
+//  Copyright (c) 2015年 陈震. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git test/git test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git test
+//
+//  Created by 陈震 on 15/10/8.
+//  Copyright (c) 2015年 陈震. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
